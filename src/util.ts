b9cfe41a8a1be8dccba1cabb1fcba7f98ca4abf4
yutoVR <yuto.fushimi@icloud.com>
2021-05-27 21:49:37 +0900

    ヘルパー関数の分離

---
+++ src/util.ts
@@ -0,0 +1,19 @@
+﻿function find<T extends HTMLElement>(query: string) {
+	let result = document.querySelector<T>(query);
+	if (result == null) {
+		console.log(`オブジェクトが見つかりませんでした: ${query}`);
+		throw new Error("エラー");
+	} else {
+		return result;
+	}
+}
+
+function findAll<T extends HTMLElement>(query: string) {
+	let result = document.querySelectorAll<T>(query);
+	if (result == null) {
+		console.log(`オブジェクトが見つかりませんでした: ${query}`);
+		throw new Error("エラー");
+	} else {
+		return result;
+	}
+}
